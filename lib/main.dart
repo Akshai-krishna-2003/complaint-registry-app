c0a7968a167e9f82929dab64eb826937841c2c78
Akshai-krishna-2003 <akshaykrishna1983@gmail.com>
2026-05-10 16:37:03 +0530

    feat: make the system navigation bar transparent with dark icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // main.dart
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:registry/src/common/app_theme.dart';
 import 'package:registry/src/features/auth/presentation/auth_screen.dart';
@@ -11,9 +12,20 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Make the system navigation bar transparent with dark icons
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarDividerColor: Colors.transparent,
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
   await dotenv.load(fileName: ".env");
 
-  await Firebase.initializeApp(); 
+  await Firebase.initializeApp();
 
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
